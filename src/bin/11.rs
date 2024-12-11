478586f2b6a8ebaf0623e750997dbfa878524c98
tejp <anton@ndersson.co>
2024-12-11 09:14:47 +0100

    day 11 finalized

--- src/bin/11.rs
+++ src/bin/11.rs
@@ -1,40 +1,37 @@
-fn main() {
-    let binding = aoc24::input(11);
-    let input: Vec<_> = binding.trim().split(' ').map(|n| n.parse::<u64>().unwrap()).collect();
+use std::collections::HashMap;
 
-    /*let part1 = input.into_iter().fold(0, |a, n| {
-        let mut v = vec![n];
-        for _ in 0..75 {
-            let mut new_v = vec![];
-            for n in v {
-                match n {
-                    0 => new_v.push(1),
-                    n if format!("{n}").len() % 2 == 0 => {
-                        let n_ = format!("{n}");
-                        let (n1, n2) = n_.split_at(n_.len() / 2);
-                        new_v.push(n1.parse::<u64>().unwrap());
-                        new_v.push(n2.parse::<u64>().unwrap());
-                    }
-                    n => new_v.push(n * 2024),
-                }
-            }
-            v = new_v;
+fn calc(stone: u64, blinks_left: u32, cache: &mut HashMap<(u64, u32), u64>) -> u64 {
+    if blinks_left == 0 {
+        return 1;
+    }
+    if let Some(&n) = cache.get(&(stone, blinks_left)) {
+        return n;
+    }
+    let n = match stone {
+        0 => calc(1, blinks_left - 1, cache),
+        n if (n.ilog10() + 1) % 2 == 0 => {
+            let s = format!("{n}");
+            let (s1, s2) = s.split_at((n.ilog10() + 1) as usize / 2);
+            let (n1, n2) = (s1.parse::<u64>().unwrap(), s2.parse::<u64>().unwrap());
+            calc(n1, blinks_left - 1, cache) + calc(n2, blinks_left - 1, cache)
         }
-        a + v.len()
-    });*/
-    let part1 = (0..75).fold(input, |v: dyn Iterator, _| {
-        v.flat_map(|n| {
-            match n {
-                0 => vec![1].iter(),
-                n if format!("{n}").len() % 2 == 0 => {
-                    let n_ = format!("{n}");
-                    let (n1, n2) = n_.split_at(n_.len() / 2);
-                    vec![n1.parse::<u64>().unwrap(), n2.parse::<u64>().unwrap()].iter()
-                }
-                n => {vec![n * 2024].iter()}
-            }
-        })
-    }).count();
-    
-    println!("{:?}", part1);
+        n => calc(n * 2024, blinks_left - 1, cache),
+    };
+    cache.insert((stone, blinks_left), n);
+    n
+}
+
+fn main() {
+    let input: Vec<u64> = aoc24::input(11)
+        .split(' ')
+        .map(|n| n.trim().parse().unwrap())
+        .collect();
+
+    let mut cache: HashMap<(u64, u32), u64> = HashMap::new();
+
+    let part1: u64 = input.iter().map(|&n| calc(n, 25, &mut cache)).sum();
+    println!("{}", part1);
+
+    let part2: u64 = input.iter().map(|&n| calc(n, 75, &mut cache)).sum();
+    println!("{}", part2);
 }
